adfa979ad8ca05d0267a3aa2b205a05bb52fe313
Advait Parab <advait0012@proton.me>
2024-10-16 06:58:23 +0000

    local storage funtionality added

--- src/js/main.js
+++ src/js/main.js
@@ -9,14 +9,22 @@ const imageEl = document.querySelector("img");
 const breedListEl = document.querySelector("#data-breed-list");
 
 async function getDogsList() {
-  try {
-    const res = await fetch(`${BASE_URL}breeds/list/all`);
-    const data = await res.json();
-    return data.message;
-  } catch (err) {
-    return console.error("error occured", err);
+  let breeds = JSON.parse(localStorage.getItem("breeds"));
+
+  if (!breeds) {
+    try {
+      const res = await fetch(`${BASE_URL}breeds/list/all`);
+      const data = await res.json();
+      localStorage.setItem("breeds", JSON.stringify(data.message));
+      breeds = data.message;
+    } catch (err) {
+      console.error("Error occured", err);
+    }
   }
+
+  return breeds;
 }
+
 getDogsList();
 
 async function getDogsImage(breed) {
@@ -31,22 +39,25 @@ async function getDogsImage(breed) {
 
 async function renderSelect() {
   const dogsList = await getDogsList();
+  const fragment = document.createDocumentFragment();
   Object.keys(dogsList).forEach((dogName) => {
-    breedListEl.appendChild(Options(dogName));
+    fragment.appendChild(Options(dogName));
   });
+  breedListEl.appendChild(fragment);
 }
 
 async function renderImage(breed) {
+  imageEl.src = "lg.gif";
   const dogsImage = await getDogsImage(breed);
   imageEl.src = dogsImage;
+  imageEl.alt = breed;
 }
 
-renderImage("poodle");
+breedListEl.addEventListener("change", async (e) => {
+  const currentValue = e.target.value;
+  renderImage(currentValue);
+});
 
-renderSelect();
-
-breedListEl.addEventListener("change",async (e)=>{
-  const currentValue = e.target.value
-  renderImage(currentValue)
-  
-})
+document.addEventListener("DOMContentLoaded", () => {
+  renderSelect();
+});
